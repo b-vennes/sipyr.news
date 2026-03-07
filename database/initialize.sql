11177e294f3bbb4e797a6c49ac1d06d3bf6bff84
Branden Vennes <b.n.vennes@gmail.com>
2026-03-07 14:44:15 -0600

    adds an initialize script for the events postgresql table

---
+++ database/initialize.sql
@@ -0,0 +1,8 @@
+CREATE TABLE events (
+  id SERIAL PRIMARY KEY,
+  type_name VARCHAR(50) NOT NULL,
+  stream_name VARCHAR(100) NOT NULL,
+  stream_index INTEGER,
+  content JSON,
+  UNIQUE (type_name, stream_name, stream_index)
+);
