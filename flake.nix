cd7a44bd85070b7ad429dcb96ba157c36c336f5b
Branden <b.n.vennes@gmail.com>
2026-06-07 13:33:19 -0500

    adds nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+    description = "Projects";
+
+    inputs.flake-utils.url = "github:numtide/flake-utils";
+
+    outputs = { self, nixpkgs, flake-utils }:
+        flake-utils.lib.eachDefaultSystem (system:
+            let pkgs = nixpkgs.legacyPackages.${system}; in
+            with pkgs; {
+                devShells.default = mkShell {
+                    packages = [
+                        zsh
+                        starship
+                        neovim
+                        git
+                        github-cli
+                        mill
+                    ];
+                };
+            }
+        );
+}
